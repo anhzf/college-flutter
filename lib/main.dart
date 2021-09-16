f58fa94f36bf1d2d9a7d541a48ca82ab31b7b275
Anhzf <36941633+anhzf@users.noreply.github.com>
2021-09-16 21:16:50 +0700

    feat: :sparkles: add AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,13 @@ import 'package:flutter/material.dart';
 void main() => runApp(
       MaterialApp(
         home: Scaffold(
+          appBar: AppBar(
+            title: Text('K3519010 - Animated Container'),
+            backgroundColor: Colors.grey.shade700,
+          ),
           body: MyApp(),
           backgroundColor: Colors.grey.shade900,
         ),
-        color: Colors.white,
       ),
     );
 
