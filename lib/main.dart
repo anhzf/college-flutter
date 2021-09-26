4a2fe69335cd5c57e08879ce9b16eca6f566bcb2
Anhzf <36941633+anhzf@users.noreply.github.com>
2021-09-26 17:15:25 +0700

    feat: :sparkles: done with GradientBG, AppBar Leading, AppBar Actions

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,26 @@ void main() => runApp(
       MaterialApp(
         home: Scaffold(
           appBar: AppBar(
-            title: Text('K3519010 - Project Name'),
-            backgroundColor: Colors.grey.shade700,
+            // backgroundColor: Colors.grey.shade700,
+            leading: Icon(Icons.adb),
+            title: Text('K3519010 - AppBar Gradiasi'),
+            actions: [
+              IconButton(onPressed: () {}, icon: Icon(Icons.settings)),
+              IconButton(onPressed: () {}, icon: Icon(Icons.exit_to_app)),
+            ],
+            flexibleSpace: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    colors: [Colors.deepPurple.shade900, Colors.grey.shade900],
+                    begin: FractionalOffset.topLeft,
+                    end: FractionalOffset.bottomRight),
+                image: DecorationImage(
+                  image: AssetImage('assets/img/appbar-pattern.png'),
+                  fit: BoxFit.none,
+                  repeat: ImageRepeat.repeat,
+                ),
+              ),
+            ),
           ),
           body: MyApp(),
           backgroundColor: Colors.grey.shade900,
