50833702ba5233dd264173b60f4f9ad2f5ec764c
Anhzf <36941633+anhzf@users.noreply.github.com>
2021-09-26 19:07:12 +0700

    feat: :sparkles: done with Card Widget

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ void main() => runApp(
       MaterialApp(
         home: Scaffold(
           appBar: AppBar(
-            title: Text('K3519010 - Project Name'),
+            title: Text('K3519010 - Card Widget'),
             backgroundColor: Colors.grey.shade700,
           ),
           body: MyApp(),
@@ -18,19 +18,41 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext ctx) {
     return Center(
-      child: ElevatedButton(
-        child: Text('Klik'),
-        onPressed: () => showDialog<String>(
-          context: ctx,
-          builder: (BuildContext ctx) => AlertDialog(
-            content: Text('asdadasd'),
-            actions: [
-              TextButton(
-                child: Text('Tutup'),
-                onPressed: () => Navigator.of(ctx).pop(),
-              ),
-            ],
-          ),
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+        child: ListView(
+          children: [
+            ListItem(Icons.tune, 'General'),
+            ListItem(Icons.account_circle, 'Account'),
+            ListItem(Icons.notifications, 'Notifications'),
+            ListItem(Icons.language, 'Language & region'),
+            ListItem(Icons.info, 'About'),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ListItem extends StatelessWidget {
+  IconData icon;
+  String label;
+
+  ListItem(this.icon, this.label);
+
+  @override
+  Widget build(ctx) {
+    return Card(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 12, vertical: 16),
+        child: Row(
+          children: [
+            Icon(icon),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 8),
+              child: Text(label),
+            ),
+          ],
         ),
       ),
     );
