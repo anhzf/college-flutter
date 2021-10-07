aa89583fe04e962831f43f9994519a979717635e
Anhzf <36941633+anhzf@users.noreply.github.com>
2021-10-07 14:25:44 +0700

    feat: :sparkles: done with Media Query

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
+final appBar = AppBar(
+  title: Text('K3519010 - Media Query'),
+  backgroundColor: Colors.grey.shade700,
+);
+
 void main() => runApp(
       MaterialApp(
         home: Scaffold(
-          appBar: AppBar(
-            title: Text('K3519010 - Project Name'),
-            backgroundColor: Colors.grey.shade700,
-          ),
+          appBar: appBar,
           body: MyApp(),
           backgroundColor: Colors.grey.shade900,
         ),
@@ -17,22 +19,40 @@ void main() => runApp(
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext ctx) {
-    return Center(
-      child: ElevatedButton(
-        child: Text('Klik'),
-        onPressed: () => showDialog<String>(
-          context: ctx,
-          builder: (BuildContext ctx) => AlertDialog(
-            content: Text('asdadasd'),
-            actions: [
-              TextButton(
-                child: Text('Tutup'),
-                onPressed: () => Navigator.of(ctx).pop(),
-              ),
-            ],
-          ),
-        ),
-      ),
+    return Container(
+      child: MediaQuery.of(ctx).orientation == Orientation.portrait
+          ? Column(children: generateSquares(ctx))
+          : Row(children: generateSquares(ctx)),
     );
+  }
+
+  generateSquares(BuildContext ctx) {
+    const double DEFAULT_SQUARE_SIZE = 100;
+    final isPortrait = MediaQuery.of(ctx).orientation == Orientation.portrait;
+    final defaultWidth = isPortrait ? null : DEFAULT_SQUARE_SIZE;
+    final defaultHeight = isPortrait ? DEFAULT_SQUARE_SIZE : null;
+    final sizeRest = DEFAULT_SQUARE_SIZE * 2;
+
+    return <Widget>[
+      Container(
+        color: Colors.red,
+        width: defaultWidth,
+        height: defaultHeight,
+      ),
+      Container(
+        color: Colors.green,
+        width: isPortrait ? null : (MediaQuery.of(ctx).size.width - sizeRest),
+        height: isPortrait
+            ? (MediaQuery.of(ctx).size.height -
+                sizeRest -
+                appBar.preferredSize.height)
+            : null,
+      ),
+      Container(
+        color: Colors.blue,
+        width: defaultWidth,
+        height: defaultHeight,
+      ),
+    ];
   }
 }
