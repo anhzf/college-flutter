6dcd2135bd72523ca7d145c84c30bf9288bf5384
Anhzf <36941633+anhzf@users.noreply.github.com>
2021-10-27 00:57:07 +0700

    feat: :recycle: refactor template master

--- lib/main.dart
+++ lib/main.dart
@@ -2,34 +2,45 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text('K3519010 - Project Name'),
-            backgroundColor: Colors.grey.shade700,
-          ),
-          body: MyApp(),
-          backgroundColor: Colors.grey.shade900,
-        ),
-        color: Colors.white,
+        home: MyApp(),
+        theme: ThemeData.dark(),
       ),
     );
+
+class MainLayout extends StatelessWidget {
+  const MainLayout({Key? key, required this.child}) : super(key: key);
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('K3519010 - Project Name'),
+      ),
+      body: child,
+    );
+  }
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext ctx) {
-    return Center(
-      child: ElevatedButton(
-        child: Text('Klik'),
-        onPressed: () => showDialog<String>(
-          context: ctx,
-          builder: (BuildContext ctx) => AlertDialog(
-            content: Text('asdadasd'),
-            actions: [
-              TextButton(
-                child: Text('Tutup'),
-                onPressed: () => Navigator.of(ctx).pop(),
-              ),
-            ],
+    return MainLayout(
+      child: Center(
+        child: ElevatedButton(
+          child: Text('Klik'),
+          onPressed: () => showDialog<String>(
+            context: ctx,
+            builder: (BuildContext ctx) => AlertDialog(
+              content: Text('asdadasd'),
+              actions: [
+                TextButton(
+                  child: Text('Tutup'),
+                  onPressed: () => Navigator.of(ctx).pop(),
+                ),
+              ],
+            ),
           ),
         ),
       ),
