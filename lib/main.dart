3d11776e0593790865a05605dc28702e021fc2d8
Anhzf <36941633+anhzf@users.noreply.github.com>
2021-10-26 22:42:54 +0700

    feat: :sparkles: done with Hero & Clip Widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,37 +2,72 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text('K3519010 - Project Name'),
-            backgroundColor: Colors.grey.shade700,
-          ),
-          body: MyApp(),
-          backgroundColor: Colors.grey.shade900,
-        ),
-        color: Colors.white,
+        home: PageMain(),
       ),
     );
 
-class MyApp extends StatelessWidget {
+class PageMain extends StatelessWidget {
   @override
   Widget build(BuildContext ctx) {
-    return Center(
-      child: ElevatedButton(
-        child: Text('Klik'),
-        onPressed: () => showDialog<String>(
-          context: ctx,
-          builder: (BuildContext ctx) => AlertDialog(
-            content: Text('asdadasd'),
-            actions: [
-              TextButton(
-                child: Text('Tutup'),
-                onPressed: () => Navigator.of(ctx).pop(),
-              ),
-            ],
+    return MainLayout(
+      child: GestureDetector(
+        onTap: () {
+          Navigator.push(
+            ctx,
+            MaterialPageRoute(builder: (ctx) => PageSecond()),
+          );
+        },
+        child: Hero(
+          tag: 'avatar',
+          child: Avatar(size: 100),
+        ),
+      ),
+    );
+  }
+}
+
+class PageSecond extends StatelessWidget {
+  @override
+  Widget build(BuildContext ctx) {
+    return MainLayout(
+      child: Center(
+        child: GestureDetector(
+          onTap: () {},
+          child: Hero(
+            tag: 'avatar',
+            child: Avatar(size: 200),
           ),
         ),
       ),
     );
   }
 }
+
+class MainLayout extends StatelessWidget {
+  MainLayout({Key? key, this.child}) : super(key: key);
+
+  final Widget? child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('K3519010 - Hero Animation'),
+        backgroundColor: Colors.grey.shade700,
+      ),
+      body: child,
+      backgroundColor: Colors.grey.shade900,
+    );
+  }
+}
+
+Widget Avatar({required double size}) => ClipOval(
+      child: Container(
+        width: size,
+        height: size,
+        child: Image.network(
+          'https://www.thispersondoesnotexist.com/image',
+          fit: BoxFit.scaleDown,
+        ),
+      ),
+    );
