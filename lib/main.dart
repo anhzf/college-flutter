828f2d0025c55b5b8110c8ae068dbbc75028e512
Anhzf <36941633+anhzf@users.noreply.github.com>
2021-10-26 23:44:02 +0700

    feat: :sparkles: done with custom TabBar

--- lib/main.dart
+++ lib/main.dart
@@ -2,46 +2,56 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: DefaultTabController(
-          length: 4,
-          child: Scaffold(
-            appBar: AppBar(
-              title: Text('K3519010 - AppBar with Tabs'),
-              bottom: TabBar(
-                tabs: [
-                  Tab(
-                    icon: Icon(Icons.home_filled),
-                  ),
-                  Tab(
-                    icon: Icon(Icons.search),
-                  ),
-                  Tab(
-                    icon: Icon(Icons.favorite),
-                  ),
-                  Tab(
-                    icon: Icon(Icons.person),
-                  ),
-                ],
-              ),
-            ),
-            body: TabBarView(
-              children: [
-                Center(
-                  child: Text('Home'),
-                ),
-                Center(
-                  child: Text('Search and Explore'),
-                ),
-                Center(
-                  child: Text('All notifications will be here'),
-                ),
-                Center(
-                  child: Text('My Account'),
-                ),
-              ],
-            ),
-          ),
-        ),
+        home: PageMain(),
         theme: ThemeData.dark(),
       ),
     );
+
+class PageMain extends StatelessWidget {
+  const PageMain({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final tabBar = TabBar(
+      tabs: [
+        Tab(
+          icon: Icon(Icons.home_filled),
+        ),
+        Tab(
+          icon: Icon(Icons.person),
+        ),
+      ],
+      indicator: BoxDecoration(
+        color: Colors.green.withOpacity(.6),
+        border: Border(
+          top: BorderSide(color: Colors.red, width: 4),
+        ),
+      ),
+    );
+
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('K3519010 - Custom TabBar in AppBar'),
+          bottom: PreferredSize(
+            child: tabBar,
+            preferredSize: Size.fromHeight(
+              tabBar.preferredSize.height,
+            ),
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            Center(
+              child: Text('Home'),
+            ),
+            Center(
+              child: Text('My Account'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
