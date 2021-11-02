48610a0a9bb26ae17dd4ca6cebd3768f3103e94f
Anhzf <36941633+anhzf@users.noreply.github.com>
2021-11-02 14:39:45 +0700

    feat: :sparkles: done with converter

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,17 @@
+import 'package:college_flutter_app/utils/converter.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+const measures = [
+  'meters',
+  'kilometers',
+  'grams',
+  'kilograms',
+  'feet',
+  'miles',
+  'pounds (lbs)',
+  'ounces',
+];
 
 void main() => runApp(
       MaterialApp(
@@ -16,34 +29,136 @@ class MainLayout extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('K3519010 - Project Name'),
+        title: const Text('K3519010 - Measure Converter'),
       ),
-      body: child,
+      body: Container(
+        child: SingleChildScrollView(child: child),
+        padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 16),
+      ),
     );
   }
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
-  Widget build(BuildContext ctx) {
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  static const gapY = SizedBox(height: 24);
+  final _formKey = GlobalKey<FormState>();
+  ConversionData conversionData = new ConversionData();
+  String resultMsg = '';
+
+  @override
+  Widget build(BuildContext context) {
     return MainLayout(
-      child: Center(
-        child: ElevatedButton(
-          child: Text('Klik'),
-          onPressed: () => showDialog<String>(
-            context: ctx,
-            builder: (BuildContext ctx) => AlertDialog(
-              content: Text('asdadasd'),
-              actions: [
-                TextButton(
-                  child: Text('Tutup'),
-                  onPressed: () => Navigator.of(ctx).pop(),
+      child: Form(
+        key: _formKey,
+        autovalidateMode: AutovalidateMode.always,
+        child: Column(
+          children: [
+            // Field
+            Text('Values', style: Theme.of(context).textTheme.bodyText1),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 4),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Insert value to convert',
+                  icon: Icon(Icons.input),
+                  filled: true,
                 ),
-              ],
+                keyboardType: TextInputType.number,
+                validator: (v) {
+                  return double.tryParse(v!) == null
+                      ? 'Failed to parse value'
+                      : null;
+                },
+                onChanged: (v) {
+                  setState(() {
+                    conversionData.numberFrom = double.parse(v);
+                  });
+                },
+              ),
             ),
-          ),
+            gapY,
+            Text('From', style: Theme.of(context).textTheme.bodyText1),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 4),
+              child: DropdownButtonFormField(
+                value: conversionData.measureFrom,
+                isExpanded: true,
+                items: measures
+                    .map((e) => DropdownMenuItem(
+                          value: e,
+                          child: Text(e),
+                        ))
+                    .toList(),
+                decoration: InputDecoration(
+                  filled: true,
+                  icon: Icon(Icons.swap_horiz),
+                ),
+                onChanged: (v) => setState(() {
+                  conversionData.measureFrom = v.toString();
+                }),
+              ),
+            ),
+            gapY,
+            Text('To', style: Theme.of(context).textTheme.bodyText1),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 4),
+              child: DropdownButtonFormField(
+                value: conversionData.measureTo,
+                isExpanded: true,
+                items: measures
+                    .map((e) => DropdownMenuItem(
+                          value: e,
+                          child: Text(e),
+                        ))
+                    .toList(),
+                decoration: InputDecoration(
+                  filled: true,
+                  icon: Icon(Icons.swap_horiz),
+                ),
+                onChanged: (v) => setState(() {
+                  conversionData.measureTo = v.toString();
+                }),
+              ),
+            ),
+            gapY,
+            ElevatedButton(
+              child: const Text('CONVERT'),
+              onPressed: onConvertClick,
+            ),
+            gapY,
+            Text(
+              resultMsg,
+              style: Theme.of(context).textTheme.headline6,
+            )
+          ],
         ),
       ),
     );
   }
+
+  void onConvertClick() {
+    setState(() {
+      if (conversionData.result != null) {
+        resultMsg =
+            '${conversionData.numberFrom} ${conversionData.measureFrom} are ${conversionData.result} ${conversionData.measureTo}';
+      } else {
+        resultMsg = 'This conversion cannot be performed';
+      }
+    });
+  }
+}
+
+class ConversionData {
+  double numberFrom = 0;
+  String measureFrom = 'meters';
+  String measureTo = 'meters';
+
+  double? get result => Converter.convert(numberFrom, measureFrom, measureTo);
 }
---
+++ lib/utils/converter.dart
@@ -0,0 +1,41 @@
+class Converter {
+  static const formulas = {
+    '0': [1, 0.001, 0, 0, 3.28084, 0.000621371, 0, 0],
+    '1': [1000, 1, 0, 0, 3280.84, 0.621371, 0, 0],
+    '2': [0, 0, 1, 0.0001, 0, 0, 0.00220462, 0.035274],
+    '3': [0, 0, 1000, 1, 0, 0, 2.20462, 35.274],
+    '4': [0.3048, 0.0003048, 0, 0, 1, 0.000189394, 0, 0],
+    '5': [1609.34, 1.60934, 0, 0, 5280, 1, 0, 0],
+    '6': [0, 0, 453.592, 0.453592, 0, 0, 1, 16],
+    '7': [0, 0, 28.3495, 0.0283495, 3.28084, 0, 0.0625, 1],
+  };
+  static const Map<String, int> measures = {
+    'meters': 0,
+    'kilometers': 1,
+    'grams': 2,
+    'kilograms': 3,
+    'feet': 4,
+    'miles': 5,
+    'pounds (lbs)': 6,
+    'ounces': 7,
+  };
+
+  static double? convert(double value, String from, String to) {
+    final unitFrom = measures[from];
+    final unitTo = measures[to];
+
+    if (unitFrom != null && unitTo != null) {
+      final x = formulas[unitFrom.toString()];
+      print([value, unitFrom, unitTo, x]);
+
+      if (x != null) {
+        final multiplier = x[unitTo];
+        print(multiplier);
+
+        return value * multiplier;
+      }
+    }
+
+    return null;
+  }
+}
